61a9b444cad05b6463b7306570a7978cfb5d7f39
Garcia, Jacob <jgarcia@idtdna.com>
2023-08-07 10:31:12 -0500

    Created startup and the objects

---
+++ ElevatorX/Objects/Building.java
@@ -0,0 +1,12 @@
+package Objects;
+public class Building {
+    private int _floors;
+
+    public Building(int floors) {
+        _floors = floors;
+    }
+
+    public void Build() {
+        
+    }
+}
---
+++ ElevatorX/Objects/Elevator.java
@@ -0,0 +1,7 @@
+package Objects;
+public class Elevator {
+    
+    public Elevator() {
+        
+    }
+}
